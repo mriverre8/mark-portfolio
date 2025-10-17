9a74915d1f9d35be7e6cf13d8d9b068078e3d563
mriverre8 <rivemark00@gmail.com>
2025-10-17 22:10:12 +0200

    astro add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: 'es',
-        locales: ['es', 'en', 'ca'],
-        routing: { prefixDefaultLocale: true },
-    },
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  i18n: {
+      defaultLocale: 'es',
+      locales: ['es', 'en', 'ca'],
+      routing: { prefixDefaultLocale: true },
+  },
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  adapter: vercel(),
 });
